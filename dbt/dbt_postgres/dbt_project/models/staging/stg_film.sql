80ea2f862615fea1de9ac13c52c2635bb4b6d08a
Piotr Biesiada <piotrek.biesiada@outlook.com>
2025-05-19 16:31:06 +0000

    1 dzialajaca tabela

--- dbt/dbt_postgres/dbt_project/models/staging/stg_film.sql
+++ dbt/dbt_postgres/dbt_project/models/staging/stg_film.sql
@@ -1,6 +1,5 @@
--- Na początek wymuszamy załadowanie rozszerzenia
-
-select * from postgres(
+select * from postgres_scan(
   'host=172.17.0.3 user=sakila password=p_ssW0rd dbname=sakila port=5432',
-  'public.film'
+  'public',
+  'film'
 )
