3f5517d1b397a4e30131acd18f26537560ea16b0
Piotr Biesiada <piotrek.biesiada@outlook.com>
2025-05-25 07:24:19 +0000

    rm unecessary objects

--- dbt/dbt_postgres/dbt_project/models/staging/stg_rental.sql
+++ dbt/dbt_postgres/dbt_project/models/staging/stg_rental.sql
@@ -1,5 +1,5 @@
 select * from postgres_scan(
-  'host=172.17.0.2 user=sakila password=p_ssW0rd dbname=sakila port=5432',
+  'host=172.17.0.3 user=sakila password=p_ssW0rd dbname=sakila port=5432',
   'public',
   'rental'
 )
--- dbt/lab-dbt05/airflow/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p /config/airflow/dags/
-cp airflow.cfg /config/airflow/airflow.cfg
-cp webserver_config.py /config/airflow/webserver_config.py
-cp -R dags/* /config/airflow/dags
-airflow db migrate
-airflow standalone
